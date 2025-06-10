a5aa997b5837b59e199173f5af7f361308a8747a
eratead <michael@erate.co.il>
2025-06-10 09:48:41 +0300

    added initial users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,8 +16,21 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Michael',
             'email' => 'gurovm@gmail.com',
-            'password' => Hash::make('matsgm'),
+            'password' => Hash::make('ts3w4dpq'),
             'is_admin' => true,
+        ]);
+        User::create([
+            'name' => 'Eran',
+            'email' => 'eran@erate.co.il',
+            'password' => Hash::make('t48r2ue8'),
+            'is_admin' => true,
+        ]);
+        User::create([
+            'name' => 'Eldad',
+            'email' => 'eldad@alea-m.com',
+            'password' => Hash::make('9s7ez5rj'),
+            'is_admin' => false,
+            'is_report' => true,
         ]);
     }
 }
