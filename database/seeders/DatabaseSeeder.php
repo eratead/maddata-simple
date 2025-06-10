426ee0b3e68576b453185d0fd90fdcd50e4c79f6
eratead <michael@erate.co.il>
2025-06-10 09:41:13 +0300

    remove initial seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            InitialSeed::class,
+            // InitialSeed::class,
         ]);
     }
 }
