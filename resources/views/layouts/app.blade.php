024c016a2e18045e95c2a7c5e8ee369a4840c214
eratead <michael@erate.co.il>
2025-07-08 14:22:16 +0300

    name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>MadData dashboard</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
 
         <!-- Fonts -->
